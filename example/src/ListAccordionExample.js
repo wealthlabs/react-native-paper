57e6a56243936cacf42c4bc906f8e48691c2c1bb
Hellinton Eduardo Klein <hellinton.klein@wssim.com.br>
2019-05-09 10:30:18 -0300

    Chore: 57639 - Accordion com controle externo, reset example

--- example/src/ListAccordionExample.js
+++ example/src/ListAccordionExample.js
@@ -2,27 +2,21 @@
 
 import * as React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
-import { List, Divider, withTheme } from 'react-native-paper';
+import {
+  ListAccordion,
+  ListSection,
+  ListItem,
+  Divider,
+  withTheme,
+} from 'react-native-paper';
 import type { Theme } from 'react-native-paper/types';
 
 type Props = {
   theme: Theme,
 };
 
-type State = {
-  expanded: boolean,
-};
-
-class ListAccordionExample extends React.Component<Props, State> {
-  static title = 'List.Accordion';
-
-  state = {
-    expanded: true,
-  };
-
-  _handlePress = () => {
-    this.setState({ expanded: !this.state.expanded });
-  };
+class ListAccordionExample extends React.Component<Props> {
+  static title = 'ListAccordion';
 
   render() {
     const {
@@ -30,52 +24,31 @@ class ListAccordionExample extends React.Component<Props, State> {
         colors: { background },
       },
     } = this.props;
-
     return (
       <ScrollView style={[styles.container, { backgroundColor: background }]}>
-        <List.Section title="Expandable list item">
-          <List.Accordion
-            left={props => <List.Icon {...props} icon="folder" />}
-            title="Expandable list item"
-          >
-            <List.Item title="List item 1" />
-            <List.Item title="List item 2" />
-          </List.Accordion>
-          <List.Accordion
-            left={props => <List.Icon {...props} icon="folder" />}
-            title="Start expanded"
-            expanded={this.state.expanded}
-            onPress={this._handlePress}
-          >
-            <List.Item title="List item 1" />
-          </List.Accordion>
-        </List.Section>
+        <ListSection title="Expandable list item">
+          <ListAccordion icon="folder" title="Expandable list item">
+            <ListItem title="List item 1" />
+            <ListItem title="List item 2" />
+          </ListAccordion>
+        </ListSection>
         <Divider />
-        <List.Section title="Expandable & multiline list item">
-          <List.Accordion
+        <ListSection title="Expandable & multiline list item">
+          <ListAccordion
             title="Expandable list item"
             description="Describes the expandable list item"
           >
-            <List.Item title="List item 1" />
-            <List.Item title="List item 2" />
-          </List.Accordion>
-        </List.Section>
+            <ListItem title="List item 1" />
+            <ListItem title="List item 2" />
+          </ListAccordion>
+        </ListSection>
         <Divider />
-        <List.Section title="Expandable list with icons">
-          <List.Accordion
-            left={props => <List.Icon {...props} icon="star" />}
-            title="Accordion item 1"
-          >
-            <List.Item
-              left={props => <List.Icon {...props} icon="thumb-up" />}
-              title="List item 1"
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon="thumb-down" />}
-              title="List item 2"
-            />
-          </List.Accordion>
-        </List.Section>
+        <ListSection title="Expandable list with icons">
+          <ListAccordion icon="star" title="Accordion item 1">
+            <ListItem icon="thumb-up" title="List item 1" />
+            <ListItem icon="thumb-down" title="List item 2" />
+          </ListAccordion>
+        </ListSection>
       </ScrollView>
     );
   }
